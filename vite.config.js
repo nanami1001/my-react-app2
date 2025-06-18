1ee7b202ae75cbabbd97b81603a4b46609454290
nanami1001 <5b1g0031@stust.edu.tw>
2025-06-18 14:39:39 +0800

    ver2.1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/my-react-app2/', // 這裡要填你 GitHub repo 的名稱，注意開頭和結尾的斜線
   plugins: [react()],
 })
